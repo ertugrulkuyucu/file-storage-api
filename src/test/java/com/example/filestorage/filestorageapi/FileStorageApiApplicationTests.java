d9b540ca529457175f065f27bb21af93092203f6
ertugrulkuyucu <ertugrul_kuyucu@hotmail.com>
2023-06-23 06:03:26 +0300

    Project generated

---
+++ src/test/java/com/example/filestorage/filestorageapi/FileStorageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filestorage.filestorageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
